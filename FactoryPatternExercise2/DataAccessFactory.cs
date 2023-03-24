59cbf0aefb2fc342adf8518d715633b0e9a0608f
juan lopez salamanca <juanlopezsalamanca1@gmail.com>
2023-03-24 13:17:02 -0500

    completed

---
+++ FactoryPatternExercise2/DataAccessFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    public static class DataAccessFactory
+    {
+        public static IDataAccess GetDataAccessType(string databasetype)
+        {
+            switch (databasetype.ToLower())
+            {
+                case "list":
+                    return new ListDataAccess();
+
+                case "sql":
+                    return new SQLDataAccess();
+
+                case "mongo":
+                    return new MongoDataAccess();
+
+                default:
+                    return new ListDataAccess();
+            }
+        }
+    }
+}
--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, what data base would you like to use: List, SQL, Mongo");
+            string dataBaseType = Console.ReadLine();
+            IDataAccess database = DataAccessFactory.GetDataAccessType(dataBaseType);
+            database.LoadData();
+            database.SaveData();
         }
     }
 }
